d9d122c8d25dcd480ca67d222396e422b248cd82
katiemiao <katiemiao61@gmail.com>
2014-09-03 21:34:22 -0400

    commented out to deactivate

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require 'bundler' #require bundler
-Bundler.require #require everything in bundler in gemfile
-require 'pry'
-require './lib/Mailgun_final_project.rb'
-require './lib/twilio_final_project.rb'
+# require 'bundler' #require bundler
+# Bundler.require #require everything in bundler in gemfile
+# require 'pry'
+# require './lib/Mailgun_final_project.rb'
+# require './lib/twilio_final_project.rb'
 
-get '/' do
-  @weather_today = Weather.new
-  erb :index #this tells your program to use the html associated with the index.html.erb file in your browser
-end   
+# get '/' do
+#   @weather_today = Weather.new
+#   erb :index #this tells your program to use the html associated with the index.html.erb file in your browser
+# end   
--- lib/final_project.rb
+++ lib/final_project.rb
@@ -1,18 +1,18 @@
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
 
-html = "http://www.weather.com/weather/today/USCT0085:1"
+# html = "http://www.weather.com/weather/today/USCT0085:1"
 
-doc = Nokogiri::HTML(open(html))
+# doc = Nokogiri::HTML(open(html))
 
-right_now_temperature = doc.css('.wx-temperature').children.children.first.text
-right_now_forecast = doc.css('.wx-phrase').children.first.text
-past_24_hr_precipitation = doc.css('.wx-data').children.first.text
+# right_now_temperature = doc.css('.wx-temperature').children.children.first.text
+# right_now_forecast = doc.css('.wx-phrase').children.first.text
+# past_24_hr_precipitation = doc.css('.wx-data').children.first.text
 
-today_temperature = doc.css('.wx-temperature').children[2].text
-today_forecast = doc.css('.wx-phrase').children[1].text
-today_rain = doc.css('.wx-data').children[2].text
+# today_temperature = doc.css('.wx-temperature').children[2].text
+# today_forecast = doc.css('.wx-phrase').children[1].text
+# today_rain = doc.css('.wx-data').children[2].text
 
-tonight_temperature = doc.css('.wx-temperature').children[4].text
-tonight_forecast = doc.css('.wx-phrase').children[2].text
-tonight_rain = doc.css('.wx-data').children[4].text
+# tonight_temperature = doc.css('.wx-temperature').children[4].text
+# tonight_forecast = doc.css('.wx-phrase').children[2].text
+# tonight_rain = doc.css('.wx-data').children[4].text
